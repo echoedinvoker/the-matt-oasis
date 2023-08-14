7b983be9de836301a46604b4ca134fafaa846822
matt <echoedinvoker@gmail.com>
2023-08-14 10:23:06 +0800

    fake login switch

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -5,13 +5,28 @@ import Input from "../../ui/Input";
 import FormRowVertical from "../../ui/FormRowVertical";
 import { useLogin } from "./useLogin";
 import SpinnerMini from "../../ui/SpinnerMini";
+import styled from "styled-components"
+
+const SwitchButton = styled(Button)`
+  letter-spacing: 2px;
+  text-transform: uppercase;
+`
 
 function LoginForm({ onSignup }) {
-  const [email, setEmail] = useState("echoeinvoker@gmail.com");
+  const [email, setEmail] = useState("rrr@rrr.rr");
   const [password, setPassword] = useState("qwer1234");
   // const [email, setEmail] = useState("");
   // const [password, setPassword] = useState("");
   const { login, isLoading } = useLogin()
+
+  function handleToggleFakeAccounts() {
+    if (email === 'rrr@rrr.rr') {
+      setEmail('ragehiy649@viperace.com')
+    }
+    else {
+      setEmail('rrr@rrr.rr')
+    }
+  }
 
   function handleSubmit(e) {
     e.preventDefault()
@@ -26,6 +41,12 @@ function LoginForm({ onSignup }) {
 
   return (
     <Form onSubmit={handleSubmit}>
+      <SwitchButton type='button' $size='small' $variation='danger' onClick={handleToggleFakeAccounts}>
+        {email === 'rrr@rrr.rr'
+          ? 'GUEST'
+          : 'STAFF'
+        }
+      </SwitchButton>
       <FormRowVertical label="Email address">
         <Input
           type="email"
