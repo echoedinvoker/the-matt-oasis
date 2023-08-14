82e662549dc950320e4f25f0dd28413386c07e7f
matt <echoedinvoker@gmail.com>
2023-08-14 18:44:55 +0800

    tag in Home activities of guest

--- src/ui/UserBookingItem.jsx
+++ src/ui/UserBookingItem.jsx
@@ -1,6 +1,7 @@
 import { differenceInDays } from "date-fns"
 import styled from "styled-components"
 import Pay from "./Pay"
+import Tag from "./Tag"
 
 const List = styled.div`
   display: flex;
@@ -30,24 +31,24 @@ function UserBookingItem({ booking }) {
         (
           daysToStart > 0
             ? <Row>
-              <p>Cabin {booking.cabins.name} has {daysToStart} for check-in</p>
+              <Tag type='blue'>Wait</Tag><p>Cabin {booking.cabins.name} has {daysToStart} for check-in</p>
               {!booking.isPaid && <Pay size='small' id={booking.id} />}
             </Row>
             : daysToStart === 0
               ? <Col>
-              <p>Cheking in to Cabin {booking.cabins.name} tody</p>
+              <Tag type='green'>Today</Tag><p>Cheking in to Cabin {booking.cabins.name} tody</p>
               {!booking.isPaid && <Pay size='small' id={booking.id} />}
               </Col>
-              : <p>The reservation for Cabin {booking.cabins.name} has expired</p>
+              : <Row><Tag type='red'>Expired</Tag><p>The reservation for Cabin {booking.cabins.name} has expired</p></Row>
         )
       }
       {booking.status === 'checked-in' &&
         (
           daysToEnd > 0
-            ? <p>Cabin {booking.cabins.name} has {daysToEnd} days remaining until check-out</p>
+            ? <Row><Tag type='blue'>Stay</Tag><p>Cabin {booking.cabins.name} has {daysToEnd} days remaining until check-out</p></Row>
             : daysToEnd === 0
-              ? <p>Cheking out to Cabin {booking.cabins.name} tody</p>
-              : <p>Cabin {booking.cabins.name} has overdue check-out</p>
+              ? <Row><Tag type='green'>Today</Tag><p>Cheking out to Cabin {booking.cabins.name} tody</p></Row>
+              : <Row><Tag type='red'>Expired</Tag><p>Cabin {booking.cabins.name} has overdue check-out</p></Row>
         )
       }
     </List>
