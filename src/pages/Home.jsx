42394e73ed760303f00c076112b2d41885ca0ff6
matt <echoedinvoker@gmail.com>
2023-08-14 08:42:43 +0800

    Description

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,47 +4,48 @@ import ActivitiesSlider from "../ui/ActivitiesSlider"
 import { useCabins } from "../features/cabins/useCabins"
 import Spinner from "../ui/Spinner"
 import { styled } from "styled-components"
+import Description from "../ui/Description"
 
-const Description = styled.p`
-  font-family: 'Georgia', serif;
-  font-size: 18px; /* 或其他合適的大小 */
-  line-height: 1.5; /* 調整行高 */
-  font-style: italic;
-  color: var(--color-grey-500);
-`
+// const Description = styled.p`
+//   font-family: 'Georgia', serif;
+//   font-size: 18px; /* 或其他合適的大小 */
+//   line-height: 1.5; /* 調整行高 */
+//   font-style: italic;
+//   color: var(--color-grey-500);
+// `
 
 function Home() {
   const { cabins, isLoading } = useCabins()
   const [cabinId, setCabinId] = useState(0)
-  const [displayedText, setDisplayedText] = useState('')
-  const [index, setIndex] = useState(0)
+  // const [displayedText, setDisplayedText] = useState('')
+  // const [index, setIndex] = useState(0)
 
   if (isLoading) <Spinner />
 
   const description = cabinId > 0 && cabins?.find(cabin => cabin.id === cabinId).description
 
-  useEffect(() => {
-    setDisplayedText(''); // 重置顯示的文字
-    setIndex(0); // 重置索引
-  }, [description]);
+  // useEffect(() => {
+  //   setDisplayedText(''); // 重置顯示的文字
+  //   setIndex(0); // 重置索引
+  // }, [description]);
 
-  useEffect(() => {
-    if (index < description.length) {
-      const timer = setTimeout(() => {
-        setDisplayedText(prevText => prevText + description[index])
-        setIndex(prevIndex => prevIndex + 1)
-      }, 100)
+  // useEffect(() => {
+  //   if (index < description.length) {
+  //     const timer = setTimeout(() => {
+  //       setDisplayedText(prevText => prevText + description[index])
+  //       setIndex(prevIndex => prevIndex + 1)
+  //     }, 100)
 
-      return () => clearTimeout(timer)
-    }
+  //     return () => clearTimeout(timer)
+  //   }
 
-  }, [index, description])
+  // }, [index, description])
 
   return (
     <>
       <CabinSlick onChange={setCabinId} />
       <ActivitiesSlider />
-      <Description>{displayedText}</Description>
+      <Description text={description} />
     </>
   )
 }
---
+++ src/ui/Description.jsx
@@ -0,0 +1,39 @@
+import { useEffect, useState } from "react";
+import { styled } from "styled-components"
+
+const StyledDescription = styled.p`
+  font-family: 'Georgia', serif;
+  font-size: 18px; 
+  line-height: 1.5;
+  font-style: italic;
+  color: var(--color-grey-500);
+`
+
+function Description({ text }) {
+  const [displayedText, setDisplayedText] = useState('')
+  const [index, setIndex] = useState(0)
+
+
+  useEffect(() => {
+    setDisplayedText('');
+    setIndex(0);
+  }, [text]);
+
+  useEffect(() => {
+    if (index < text.length) {
+      const timer = setTimeout(() => {
+        setDisplayedText(prevText => prevText + text[index])
+        setIndex(prevIndex => prevIndex + 1)
+      }, 100)
+
+      return () => clearTimeout(timer)
+    }
+
+  }, [index, text])
+
+  return <StyledDescription>
+    {displayedText}
+  </StyledDescription>
+}
+
+export default Description
